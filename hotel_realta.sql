67457f0d8f8922ed77be1725612ea03b9c09b198
Mahendrata Harpi <justharpi@gmail.com>
2022-12-27 17:38:38 +0700

    formatting writting convetion

--- hotel_realta.sql
+++ hotel_realta.sql
@@ -1,91 +1,107 @@
-CREATE DATABASE Hotel_Realta
-
-USE Hotel_Realta
-
-GO
-CREATE SCHEMA [master] ;
+-- membuat database
+USE tempdb;
 GO
 
-create table Master.regions(
-region_code int identity(1,1),
-region_name nvarchar(35) unique not null,
-constraint pk_region_code primary key(region_code)
-);
-create table Master.country(
-country_id int identity(1,1),
-country_name nvarchar (55) unique not null,
-country_region_id int,
-constraint pk_country_id primary key (country_id),
-constraint fk_country_region_id foreign key(country_region_id) references Master.regions(region_code) on delete cascade on update cascade
+DROP DATABASE IF EXISTS Hotel_Realta;
+GO
 
-);
+CREATE DATABASE Hotel_Realta;
+GO
 
-create table Master.provinces(
-prov_id int identity (1,1),
-prov_name nvarchar (85) not null,
-prov_country_id int
-constraint pk_prov_id primary key(prov_id),
-constraint fk_prov_country_name foreign key(prov_country_id) references Master.country(country_id) on delete cascade on update cascade
+-- menggunakan db hotel_realta
+USE Hotel_Realta;
+GO
+
+CREATE SCHEMA Master;
+GO
+
+CREATE TABLE Master.regions (
+  region_code int IDENTITY(1, 1),
+  region_name nvarchar(35) UNIQUE NOT NULL,
+  CONSTRAINT pk_region_code PRIMARY KEY(region_code)
 );
 
-create table Master.address(
-addr_id int identity(1,1),
-addr_line1 nvarchar(255) not null,
-addr_line2 nvarchar(255),
-addr_postal_code nvarchar(5),
-addr_spatial_location geography,
-addr_prov_id int,
-constraint pk_addr_id primary key(addr_id),
-constraint fk_addr_prov_id foreign key(addr_prov_id) references Master.provinces(prov_id) on delete cascade on update cascade
+CREATE TABLE Master.country (
+  country_id int IDENTITY(1, 1),
+  country_name nvarchar(55) UNIQUE NOT NULL,
+  country_region_id int,
+  CONSTRAINT pk_country_id PRIMARY KEY (country_id),
+  CONSTRAINT fk_country_region_id FOREIGN KEY(country_region_id) REFERENCES Master.regions(region_code) 
+    ON DELETE CASCADE 
+    ON UPDATE CASCADE
 );
 
-create table master.category_group(
-cagro_id int identity(1,1),
-cagro_name nvarchar(25) unique not null check (cagro_name in ('1','2','3 ','4','5','6','7')),
-cagro_description nvarchar(255),
-cagro_type nvarchar(25) not null check (cagro_type in('category','service','facility')),
-cagro_icon nvarchar(255),
-cagro_icon_url nvarchar(255),
-constraint pk_cagro_id primary key(cagro_id)
+CREATE TABLE Master.provinces (
+  prov_id int IDENTITY (1, 1),
+  prov_name nvarchar(85) NOT NULL,
+  prov_country_id int CONSTRAINT pk_prov_id PRIMARY KEY(prov_id),
+  CONSTRAINT fk_prov_country_name FOREIGN KEY(prov_country_id) REFERENCES Master.country(country_id) 
+    ON DELETE CASCADE 
+    ON UPDATE CASCADE
 );
+
+CREATE TABLE Master.address (
+  addr_id int IDENTITY(1, 1),
+  addr_line1 nvarchar(255) NOT NULL,
+  addr_line2 nvarchar(255),
+  addr_postal_code nvarchar(5),
+  addr_spatial_location geography,
+  addr_prov_id int,
+  CONSTRAINT pk_addr_id PRIMARY KEY(addr_id),
+  CONSTRAINT fk_addr_prov_id FOREIGN KEY(addr_prov_id) REFERENCES Master.provinces(prov_id) 
+    ON DELETE CASCADE 
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE Master.category_group (
+  cagro_id int IDENTITY(1, 1),
+  cagro_name nvarchar(25) UNIQUE NOT NULL CHECK (cagro_name IN ('1', '2', '3 ', '4', '5', '6', '7')),
+  cagro_description nvarchar(255),
+  cagro_type nvarchar(25) NOT NULL CHECK (cagro_type IN('category', 'service', 'facility')),
+  cagro_icon nvarchar(255),
+  cagro_icon_url nvarchar(255),
+  CONSTRAINT pk_cagro_id PRIMARY KEY(cagro_id)
+);
+
 -- DROP TABLE master.category_group
-
-
-create table master.policy(
-poli_id int identity(1,1),
-poli_name nvarchar(55) not null,
-poli_description nvarchar(255),
-constraint pk_poli_id primary key(poli_id)
+CREATE TABLE Master.policy (
+  poli_id int IDENTITY(1, 1),
+  poli_name nvarchar(55) NOT NULL,
+  poli_description nvarchar(255),
+  CONSTRAINT pk_poli_id PRIMARY KEY(poli_id)
 );
 
-create table master.policy_category_group(
-poca_poli_id int  not null,
-poca_cagro_id int not null,
-constraint fk_poca_poli_id foreign key(poca_poli_id)references Master.policy(poli_id) on delete cascade on update cascade,
-constraint fk_poca_cagro_id foreign key(poca_cagro_id)references Master.category_group(cagro_id) on delete cascade on update cascade
+CREATE TABLE Master.policy_category_group (
+  poca_poli_id int NOT NULL,
+  poca_cagro_id int NOT NULL,
+  CONSTRAINT fk_poca_poli_id FOREIGN KEY(poca_poli_id) REFERENCES Master.policy(poli_id) 
+    ON DELETE CASCADE 
+    ON UPDATE CASCADE,
+  CONSTRAINT fk_poca_cagro_id FOREIGN KEY(poca_cagro_id) REFERENCES Master.category_group(cagro_id) 
+    ON DELETE CASCADE 
+    ON UPDATE CASCADE
 );
+
 -- DROP TABLE master.policy_category_group
-
-create table master.price_item(
-prit_id int identity(1,1),
-prit_name nvarchar(55) unique not null,
-prit_price money not null,
-prit_description nvarchar(255),
-prit_type nvarchar(15) not null check (prit_type in ('SNACK','FACILITY','SOFTDRINK','FOOD','SERVICE')),
-prit_modified_date datetime,
-constraint pk_prit_id primary key(prit_id)
+CREATE TABLE Master.price_item (
+  prit_id int IDENTITY(1, 1),
+  prit_name nvarchar(55) UNIQUE NOT NULL,
+  prit_price money NOT NULL,
+  prit_description nvarchar(255),
+  prit_type nvarchar(15) NOT NULL CHECK (prit_type IN ('SNACK', 'FACILITY', 'SOFTDRINK', 'FOOD', 'SERVICE')),
+  prit_modified_date datetime,
+  CONSTRAINT pk_prit_id PRIMARY KEY(prit_id)
 );
+
 -- DROP TABLE master.price_item
-
-create table master.service_task(
-seta_id int identity(1,1),
-seta_name nvarchar(85) unique not null,
-seta_seq smallint
-constraint pk_set_id primary key(seta_id) 
+CREATE TABLE Master.service_task (
+  seta_id int IDENTITY(1, 1),
+  seta_name nvarchar(85) UNIQUE NOT NULL,
+  seta_seq smallint CONSTRAINT pk_set_id PRIMARY KEY(seta_id)
 );
 
-create table master.members(
-memb_name nvarchar(15) not null,
-memb_description nvarchar(100),
-constraint pk_memb_name primary key(memb_description)
+CREATE TABLE Master.members (
+  memb_name nvarchar(15) NOT NULL,
+  memb_description nvarchar(100),
+  CONSTRAINT pk_memb_name PRIMARY KEY(memb_description)
 );

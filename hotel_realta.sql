2666df1cfa4bfb15cd8fd90d872794dc2c7dd419
Mahendrata Harpi <justharpi@gmail.com>
2023-01-03 13:31:18 +0700

    restore db

--- hotel_realta.sql
+++ hotel_realta.sql
@@ -77,7 +77,7 @@ CREATE TABLE Master.address (
 
 CREATE TABLE Master.category_group (
   cagro_id int IDENTITY(1, 1),
-  cagro_name nvarchar(25) UNIQUE NOT NULL CHECK (cagro_name IN ('Room', 'Restaurant', 'Meeting Room', 'Gym', 'Aula', 'Swimming Pool', '')),
+  cagro_name nvarchar(25) UNIQUE NOT NULL,
   cagro_description nvarchar(255),
   cagro_type nvarchar(25) NOT NULL CHECK (cagro_type IN('category', 'service', 'facility')),
   cagro_icon nvarchar(255),

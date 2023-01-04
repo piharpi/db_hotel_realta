4df1e9fea506c95b56deaf27b9802d2efc6fb9b3
Ericson <ericsonwen123@gmail.com>
2023-01-04 19:08:18 +0700

    Give Cascade On Foreign Key Schema HR

--- hotel_realta.sql
+++ hotel_realta.sql
@@ -340,8 +340,8 @@ create table hr.employee (
 	emp_joro_id int not null,
 	constraint pk_emp_id primary key (emp_id),
 	constraint uq_emp_national_id unique (emp_national_id),
-	constraint fk_emp_joro_id foreign key (emp_joro_id) references hr.job_role(joro_id),
-	constraint fk_emp_id foreign key (emp_emp_id) references hr.employee(emp_id)
+	constraint fk_emp_joro_id foreign key (emp_joro_id) references hr.job_role(joro_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	constraint fk_emp_id foreign key (emp_emp_id) references hr.employee(emp_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 create table hr.employee_pay_history (
@@ -351,7 +351,7 @@ create table hr.employee_pay_history (
 	ephi_pay_frequence int,
 	ephi_modified_date datetime,
 	constraint pk_ephi_emp_id_ephi_rate_change_date primary key(ephi_emp_id, ephi_rate_change_date),
-	constraint fk_ephi_emp_id foreign key(ephi_emp_id) references hr.employee(emp_id)
+	constraint fk_ephi_emp_id foreign key(ephi_emp_id) references hr.employee(emp_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 create table hr.shift(
@@ -374,9 +374,9 @@ create table hr.employee_department_history (
 	edhi_dept_id int not null,
 	edhi_shift_id int not null,
 	constraint pk_edhi_id_edhi_emp_id primary key (edhi_id, edhi_emp_id),
-	constraint fk_edhi_emp_id foreign key(edhi_emp_id) references hr.employee(emp_id),
-	constraint fk_shift_id foreign key (edhi_shift_id) references hr.shift(shift_id),
-	constraint fk_edhi_dept_id foreign key (edhi_dept_id) references hr.department(dept_id)
+	constraint fk_edhi_emp_id foreign key(edhi_emp_id) references hr.employee(emp_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	constraint fk_shift_id foreign key (edhi_shift_id) references hr.shift(shift_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	constraint fk_edhi_dept_id foreign key (edhi_dept_id) references hr.department(dept_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 -- this table can't create after users.users
 create table hr.work_orders (
@@ -385,7 +385,7 @@ create table hr.work_orders (
 	woro_status nvarchar(15), 
 	woro_user_id int,
 	constraint pk_woro_id primary key(woro_id),
-	constraint fk_woro_user_id foreign key(woro_user_id) references users.users(user_id) -- comment alter
+	constraint fk_woro_user_id foreign key(woro_user_id) references users.users(user_id) ON DELETE CASCADE ON UPDATE CASCADE-- comment alter
 );
 
 -- this table can't create after hotel.facilites, master.service_task, and hr.work_orders
@@ -401,8 +401,8 @@ create table hr.work_order_detail (
 	wode_faci_id int,
 	wode_woro_id int,
 	constraint pk_wode_id primary key(wode_id),
-	constraint fk_woro_wode_id foreign key(wode_woro_id) references hr.work_orders(woro_id),
-	constraint fk_wode_emp_id foreign key(wode_emp_id) references hr.employee(emp_id), -- comment alter
-	constraint fk_wode_seta_id foreign key(wode_seta_id) references master.service_task(seta_id), -- comment alter
-	constraint fk_faci_id foreign key(wode_faci_id) references hotel.facilites(faci_id)-- comment alter
+	constraint fk_wode_woro_id foreign key(wode_woro_id) references hr.work_orders(woro_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	constraint fk_wode_emp_id foreign key(wode_emp_id) references hr.employee(emp_id) ON DELETE CASCADE ON UPDATE CASCADE, -- comment alter
+	constraint fk_wode_seta_id foreign key(wode_seta_id) references master.service_task(seta_id) ON DELETE CASCADE ON UPDATE CASCADE, -- comment alter
+	constraint fk_faci_id foreign key(wode_faci_id) references hotel.facilites(faci_id) ON DELETE CASCADE ON UPDATE CASCADE-- comment alter
 );

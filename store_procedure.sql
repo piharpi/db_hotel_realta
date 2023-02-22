2b163ccc760b5b1307b546789ce9c6695340e30e
Mahendrata Harpi <justharpi@gmail.com>
2023-02-22 15:52:12 +0700

    update store procedure

--- store_procedure.sql
+++ store_procedure.sql
@@ -132,3 +132,33 @@ BEGIN
 	 WHERE [patr_id] = @id
 END
 GO
+
+-- =============================================
+-- Author:		Harpi
+-- Create date: 8 January 2023
+-- Description:	Store Procedure for top up 
+-- =============================================
+CREATE PROCEDURE Payment.spTopUpTransaction
+	-- Add the parameters for the stored procedure here
+	 @source_account As nvarchar(50), 
+	 @target_account As nvarchar(50),
+	 @expense As money = 0
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	-- TOP UP
+	UPDATE Payment.user_accounts
+		 SET usac_saldo = usac_saldo - @expense,
+				 usac_modified_date = GETDATE()
+	 WHERE usac_account_number = @source_account;
+
+	UPDATE Payment.user_accounts
+		 SET usac_saldo = usac_saldo + @expense,
+				 usac_modified_date = GETDATE()
+	 WHERE usac_account_number = @target_account;
+END
+GO

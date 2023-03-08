bf187e4b05bc9b7d435cc84ac1bc0e57436651d6
alvan <alvan.ghon@gmail.com>
2023-03-08 14:30:00 +0700

    new config for db hotel, include store procedure, trigger, and dummy values

--- hotel_realta.sql
+++ hotel_realta.sql
@@ -214,96 +214,152 @@ CREATE TABLE Users.user_password (
 );
 
 --MODULE HOTELS --
-IF OBJECT_ID('Hotel.hotels', 'U') IS NOT NULL DROP TABLE Hotel.hotels
-CREATE TABLE Hotel.hotels (
-  hotel_id int IDENTITY(1, 1) NOT NULL CONSTRAINT pk_hotel_id PRIMARY KEY,
+CREATE TABLE Master.address (
+  addr_id int IDENTITY(1, 1),
+  addr_line1 nvarchar(255) NOT NULL,
+  addr_line2 nvarchar(255),
+  addr_postal_code nvarchar(5),
+  addr_spatial_location geography,
+  addr_prov_id int,
+  CONSTRAINT pk_addr_id PRIMARY KEY(addr_id),
+);
+
+--insert from master data
+CREATE TABLE Master.category_group (
+  cagro_id int IDENTITY(1, 1),
+  cagro_name nvarchar(25) UNIQUE NOT NULL,
+  cagro_description nvarchar(255),
+  cagro_type nvarchar(25) NOT NULL CHECK (cagro_type IN('category', 'service', 'facility')),
+  cagro_icon nvarchar(255),
+  cagro_icon_url nvarchar(255),
+  CONSTRAINT pk_cagro_id PRIMARY KEY(cagro_id)
+);
+
+-- Create a new table called 'Hotels' in schema 'Hotel'
+-- Drop the table if it already exists
+IF OBJECT_ID('Hotel.Hotels', 'U') IS NOT NULL
+DROP TABLE Hotel.Hotels
+-- Create the table in the specified schema
+CREATE TABLE Hotel.Hotels
+(
+  hotel_id int IDENTITY(1,1) NOT NULL CONSTRAINT hotel_id_pk PRIMARY KEY, -- primary key column
   hotel_name nvarchar(85) NOT NULL,
   hotel_description nvarchar(500) NULL,
-  hotel_rating_star smallint NULL,
+  -- BEGIN UPDATE
+  hotel_status BIT NOT NULL CHECK(hotel_status IN(0,1)),
+  hotel_reason_status nvarchar(500) NULL,
+  -- END UPDATE
+  hotel_rating_star numeric(2,1) NULL,
   hotel_phonenumber nvarchar(25) NOT NULL,
-  hotel_modified_date datetime NULL,
-  hotel_addr_id int NOT NULL,
-  CONSTRAINT fk_hotel_addr_id FOREIGN KEY (hotel_addr_id) REFERENCES Master.address(addr_id)
+  -- Primary Key
+  hotel_addr_id INT NOT NULL,
+  hotel_addr_description nvarchar(500) NULL,
+  -- Add this later, on production
+  hotel_modified_date datetime NULL, 
+  CONSTRAINT hotel_addr_id_fk FOREIGN KEY (hotel_addr_id) REFERENCES Master.Address(addr_id)
 );
 
-IF OBJECT_ID('Hotel.hotel_reviews', 'U') IS NOT NULL DROP TABLE Hotel.hotel_reviews
-CREATE TABLE Hotel.hotel_reviews (
-  hore_id int IDENTITY(1, 1) NOT NULL CONSTRAINT pk_hore_id PRIMARY KEY,
+-- Create a new table called 'Hotel_Reviews' in schema 'Hotel'
+-- Drop the table if it already exists
+IF OBJECT_ID('Hotel.Hotel_Reviews', 'U') IS NOT NULL
+DROP TABLE Hotel.Hotel_Reviews
+
+-- Create the table in the specified schema
+CREATE TABLE Hotel.Hotel_Reviews
+(
+  hore_id INT IDENTITY(1,1) NOT NULL CONSTRAINT hore_id_pk PRIMARY KEY, -- primary key column
   hore_user_review nvarchar(125) NOT NULL,
-  hore_rating bit NOT NULL CHECK(hore_rating IN(1, 2, 3, 4, 5)) DEFAULT 5,
+  hore_rating TINYINT NOT NULL CHECK(hore_rating IN(1,2,3,4,5)) DEFAULT 5,
   hore_created_on datetime NULL,
-  hore_user_id int NOT NULL,
-  hore_hotel_id int NOT NULL,
-  CONSTRAINT pk_hore_user_id FOREIGN KEY (hore_user_id) REFERENCES Users.users(user_id),
-  CONSTRAINT fk_hore_hotel_id FOREIGN KEY (hore_hotel_id) REFERENCES Hotel.hotels(hotel_id)
-	ON DELETE CASCADE
-	ON UPDATE CASCADE
+  -- FOREIGN KEY
+  hore_user_id INT NOT NULL,
+  hore_hotel_id INT NOT NULL,
+  -- Add this later, on production
+  CONSTRAINT hore_user_id_pk FOREIGN KEY (hore_user_id) REFERENCES Users.Users(user_id),
+  CONSTRAINT hore_hotel_id_fk FOREIGN KEY (hore_hotel_id) REFERENCES Hotel.Hotels(hotel_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-IF OBJECT_ID('Hotel.facilities', 'U') IS NOT NULL DROP TABLE Hotel.facilities
-CREATE TABLE Hotel.facilities (
-  faci_id INT IDENTITY(1, 1) NOT NULL CONSTRAINT pk_faci_id PRIMARY KEY,
-  -- primary key column
+
+
+-- Create a new table called 'Facilities' in schema 'Hotel'
+-- Drop the table if it already exists
+IF OBJECT_ID('Hotel.Facilities', 'U') IS NOT NULL
+DROP TABLE Hotel.Facilities
+
+-- Create the table in the specified schema
+CREATE TABLE Hotel.Facilities
+(
+  faci_id INT IDENTITY(1,1) NOT NULL CONSTRAINT faci_id_pk PRIMARY KEY, -- primary key column
   faci_name nvarchar(125) NOT NULL,
   faci_description nvarchar(255) NULL,
   faci_max_number INT NULL,
-  faci_measure_unit VARCHAR(15) NULL CHECK(faci_measure_unit IN('People', 'Beds')),
+  faci_measure_unit VARCHAR(15) NULL CHECK(faci_measure_unit IN('people','beds')),
   faci_room_number nvarchar(6) NOT NULL,
-  faci_startdate DATETIME NOT NULL,
-  faci_endate DATETIME NOT NULL,
+  faci_startdate datetime NOT NULL,
+  faci_endate datetime NOT NULL,
   faci_low_price MONEY NOT NULL,
   faci_high_price MONEY NOT NULL,
   faci_rate_price MONEY NOT NULL,
   faci_discount SMALLMONEY NULL,
   faci_tax_rate SMALLMONEY NULL,
-  faci_modified_date DATETIME NULL,
+  faci_modified_date datetime NULL,
   --FOREIGN KEY
   faci_cagro_id INTEGER NOT NULL,
   faci_hotel_id INT NOT NULL,
+  faci_user_id INT NOT NULL,
   -- UNIQUE ID
-  CONSTRAINT uq_faci_room_number UNIQUE (faci_room_number),
-  CONSTRAINT fk_faci_cagro_id FOREIGN KEY (faci_cagro_id) REFERENCES Master.category_group(cagro_id)
-	ON DELETE CASCADE
-	ON UPDATE CASCADE,
-  CONSTRAINT fk_faci_hotel_id FOREIGN KEY (faci_cagro_id) REFERENCES Hotel.hotels(hotel_id)
-	ON DELETE CASCADE
-	ON UPDATE CASCADE
+  CONSTRAINT faci_room_number_uq UNIQUE (faci_room_number),
+  -- Add this later, on production
+  CONSTRAINT faci_cagro_id_fk FOREIGN KEY (faci_cagro_id) REFERENCES Master.Category_Group(cagro_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT faci_hotel_id_fk FOREIGN KEY (faci_hotel_id) REFERENCES Hotel.Hotels(hotel_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT faci_user_id_fk FOREIGN KEY (faci_user_id) REFERENCES Users.users(user_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-IF OBJECT_ID('Hotel.facility_price_history', 'U') IS NOT NULL DROP TABLE Hotel.facility_price_history
-CREATE TABLE Hotel.facility_price_history (
-  faph_id int IDENTITY(1, 1) NOT NULL CONSTRAINT pk_faph_id PRIMARY KEY,
+
+-- Create a new table called 'Facility_Price_History' in schema 'Hotel'
+-- Drop the table if it already exists
+IF OBJECT_ID('Hotel.Facility_Price_History', 'U') IS NOT NULL
+DROP TABLE Hotel.Facility_Price_History
+
+-- Create the table in the specified schema
+CREATE TABLE Hotel.Facility_Price_History
+(
+  faph_id INT IDENTITY(1,1) NOT NULL CONSTRAINT faph_id_pk PRIMARY KEY, -- primary key column
   faph_startdate datetime NOT NULL,
   faph_enddate datetime NOT NULL,
-  faph_low_price money NOT NULL,
-  faph_high_price money NOT NULL,
-  faph_rate_price money NOT NULL,
-  faph_discount smallmoney NOT NULL,
-  faph_tax_rate smallmoney NOT NULL,
+  faph_low_price MONEY NOT NULL,
+  faph_high_price MONEY NOT NULL,
+  faph_rate_price MONEY NOT NULL,
+  faph_discount SMALLMONEY NOT NULL,
+  faph_tax_rate SMALLMONEY NOT NULL,
   faph_modified_date datetime,
-  faph_faci_id int NOT NULL,
+  -- FOREIGN KEY
+  faph_faci_id INT NOT NULL,
   faph_user_id int NOT NULL,
-  CONSTRAINT fk_faph_faci_id FOREIGN KEY (faph_faci_id) REFERENCES Hotel.facilities(faci_id)
-	ON DELETE CASCADE
-	ON UPDATE CASCADE,
-  CONSTRAINT fk_faph_user_id FOREIGN KEY (faph_user_id) REFERENCES Users.users(user_id)
-	ON DELETE CASCADE
-	ON UPDATE CASCADE
+  -- Add this later, on production
+  CONSTRAINT faph_faci_id_fk FOREIGN KEY (faph_faci_id) REFERENCES Hotel.Facilities(faci_id) ON DELETE CASCADE ON UPDATE CASCADE,
 );
 
-IF OBJECT_ID('Hotel.facility_photos', 'U') IS NOT NULL DROP TABLE Hotel.facility_photos
-CREATE TABLE Hotel.facility_photos (
-  fapho_id int IDENTITY(1, 1) NOT NULL CONSTRAINT pk_fapho_id PRIMARY KEY,
+
+-- Create a new table called 'Facility_Photos' in schema 'Hotel'
+-- Drop the table if it already exists
+IF OBJECT_ID('Hotel.Facility_Photos', 'U') IS NOT NULL
+DROP TABLE Hotel.Facility_Photos
+
+-- Create the table in the specified schema
+CREATE TABLE Hotel.Facility_Photos
+(
+  fapho_id INT IDENTITY(1,1) NOT NULL CONSTRAINT fapho_id_pk PRIMARY KEY, -- primary key column
   fapho_thumbnail_filename nvarchar(50) NULL,
   fapho_photo_filename nvarchar(50) NULL,
-  fapho_primary bit NULL CHECK(fapho_primary IN(0, 1)),
+  fapho_primary BIT NULL CHECK(fapho_primary IN(0,1)),
   fapho_url nvarchar(255) NULL,
   fapho_modified_date datetime,
-  fapho_faci_id int NOT NULL,
-  CONSTRAINT pk_fapho_faci_id FOREIGN KEY (fapho_faci_id) REFERENCES Hotel.facilities(faci_id)
-	ON DELETE CASCADE
-	ON UPDATE CASCADE
+  -- FOREIGN KEY
+  fapho_faci_id INT NOT NULL,
+  CONSTRAINT fapho_faci_id_pk FOREIGN KEY (fapho_faci_id) REFERENCES Hotel.Facilities(faci_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
 
 -- MODULE HR --
 CREATE TABLE HR.job_role (
--- store_procedure.sql
+++ store_procedure.sql
@@ -571,3 +571,28 @@ BEGIN
 	 WHERE usac_account_number = @target_account;
 END
 GO
+
+-- ===========================================================
+-- Author	  :	Alvan Ganteng
+-- Create date: 7 March 2023
+-- Description:	Store Procedure for SELECT Hotel.Hotels
+-- ===========================================================
+
+CREATE PROCEDURE [Hotel].[spSelectHotel]
+AS
+BEGIN 
+	SET NOCOUNT	ON;
+
+	SELECT 
+		hotel_id AS HotelId
+		,hotel_name AS HotelName
+		,hotel_description AS HotelDescription
+		,hotel_status AS HotelStatus
+		,hotel_reason_status AS HotelReasonStatus
+		,hotel_rating_star AS HotelRatingStar
+		,hotel_phonenumber AS HotelPhonenumber
+		,hotel_modified_date AS HotelModifiedDate
+		,hotel_addr_id AS HotelAddrId
+		,hotel_addr_description AS HotelAddrDescription
+	FROM Hotel.Hotels
+END;
